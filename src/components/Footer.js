cca0593e766212fa70c4d6731d8ed204d092328e
Muhammad Ridwan <mohridwan1707@gmail.com>
2022-07-05 12:50:58 +0700

    fix: change profile url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import { Box, Text } from "@chakra-ui/react";
 
 export const Footer = () => {
-  return (
-    <Box py={8}>
-      <Text>
-        &copy;{" "}
-        <a href="https://www.github.com/ridlwan17/" target="blank">
-          Muhammad Ridwan{" "}
-        </a>
-        | 2021 - present
-      </Text>
-    </Box>
-  );
+    return (
+        <Box py={8}>
+            <Text>
+                &copy;{" "}
+                <a href="https://www.github.com/ridhlab/" target="blank">
+                    Muhammad Ridwan{" "}
+                </a>
+                | 2021 - present
+            </Text>
+        </Box>
+    );
 };
